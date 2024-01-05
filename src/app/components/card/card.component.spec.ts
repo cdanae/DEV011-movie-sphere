56d16cea9e79a2ea680571ac9721eb3d0bba557f
danae <cdanae.rod@gmail.com>
2024-01-05 14:01:37 -0600

    test card component

--- src/app/components/card/card.component.spec.ts
+++ src/app/components/card/card.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CardComponent } from './card.component';
 
 describe('CardComponent', () => {
@@ -8,16 +7,31 @@ describe('CardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CardComponent ]
-    })
-    .compileComponents();
+      declarations: [CardComponent],
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(CardComponent);
     component = fixture.componentInstance;
+    component.movie = {
+      title: 'Movie Title',
+      release_date: '2023-01-01',
+      poster_path: '/movie-poster-path.jpg'
+    };
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('debería mostrar la información de la película en el componente', () => {
+    const cardElement: HTMLElement = fixture.nativeElement;
+    const titleElement = cardElement.querySelector('.card-title');
+    const subtitleElement = cardElement.querySelector('.card-subtitle');
+    const imageElement: HTMLImageElement | null = cardElement.querySelector('.card-image');
+
+    expect(titleElement?.textContent).toContain('Movie Title');
+    expect(subtitleElement?.textContent).toContain('2023-01-01');
+    expect(imageElement?.src).toContain('/movie-poster-path.jpg');
   });
 });
+
+
