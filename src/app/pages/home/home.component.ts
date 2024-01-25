4eab829385d8179127ac41741c5edc9cfd6b4447
danae <cdanae.rod@gmail.com>
2024-01-25 17:56:27 +0100

    fixes response

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Movie } from 'src/app/interfaces/movies';
 import { TmdbService } from 'src/app/services/tmdb.service';
+import { movies } from './response';
 
 @Component({
   selector: 'app-home',
@@ -16,9 +17,10 @@ export class HomeComponent implements OnInit {
   constructor(private tmdbService: TmdbService) { }
 
   ngOnInit(): void {
-    this.tmdbService.getDiscoverMovies().subscribe(data => {
+/*     this.tmdbService.getDiscoverMovies().subscribe(data => {
       this.procesedMovies = data.results;
-    })
+    }) */
+    this.movies = movies.results;
     //ngOnChanges, detectar cuando cambie mi valor de filtros, ejecutar la funcion applyfilter o probar con observab;e
     //escuchar tambien paginacion
   }
